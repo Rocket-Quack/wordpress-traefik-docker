25ceef748769d2eca5ba1be94eb1180d30411495
Rocket-Quack <202538874+Rocket-Quack@users.noreply.github.com>
2025-05-07 22:06:24 +0200

    Improving the shell script for .env handling in the event of already existing file

--- start.sh
+++ start.sh
@@ -9,19 +9,31 @@ SETUP_ENV="./scripts/setup-env.sh"
 
 # First the .env.example is copied
 if [ ! -f "$TARGET" ]; then
-  echo "📄 Copying $TEMPLATE to $TARGET"
+  echo "Create the .env by copying the example."
+  echo "Copying $TEMPLATE to $TARGET..."
   cp "$TEMPLATE" "$TARGET"
-else
-  echo "$TARGET already exists – keeping it."
-fi
 
-# Creating .env with input values from console
-if [ -x "$SETUP_ENV" ]; then
-  echo "Running interactive setup..."
-  "$SETUP_ENV"
+  # Creating .env with input values from console
+  if [ -x "$SETUP_ENV" ]; then
+    echo "Running interactive setup..."
+    "$SETUP_ENV"
+  else
+    echo "Error: $SETUP_ENV not found or not executable."
+    exit 1
+  fi
+
 else
-  echo "Error: $SETUP_ENV not found or not executable."
-  exit 1
+  echo "$TARGET already exists"
+  
+  # An existing .env is used or a new one is created if required
+  read -p "Do you want to overwrite the existing .env file? (y/yes/n/no): " answer
+  if [[ "$answer" == "y" || "$answer" == "Y" || "$answer" == "yes" ]]; then
+      echo "Overwriting the existing .env file"
+      "$SETUP_ENV"
+  else
+      echo "Existing .env file is used"
+  fi
+
 fi
 
 echo "Environment setup complete"
